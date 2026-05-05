1ae56c7e995d7ad8db16f2d7909a9907e4706eb0
heis-dev <egyptdogmo@gmail.com>
2026-05-05 18:31:11 +0200

    superuser creation

--- backend/izilearning/settings.py
+++ backend/izilearning/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -135,3 +136,13 @@ CORS_ALLOWED_ORIGINS = [
 
 AUTH_USER_MODEL = 'users.User'
 
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+  }
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+  }
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,11 +1,32 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
 # Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('must be an email adress')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_platform_admin', True)
+        return self.create_user(email, password, **extra_fields)
+    #e..d...@gmail.com  password=Super123@
 
 class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True)
     is_platform_admin = models.BooleanField(default=False)
+    
     USERNAME_FIELD='email'
     REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
